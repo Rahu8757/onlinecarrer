a5f23c0216dac56132256366cca6555f0bd282af
RAHUL KUMAR <RAHUL.Kumar13@ibm.com>
2021-04-06 11:01:24 +0530

    Add project files.

---
+++ onlinecarrer/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace onlinecarrer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ onlinecarrer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="onlinecarrer.MvcApplication" Language="C#" %>
